f3c3f9814eef04fcef28efe8696b10da1f54aab4
pexApred <120075705+pexApred@users.noreply.github.com>
2023-04-10 10:55:43 -0400

    added GET request for a single note

--- server.js
+++ server.js
@@ -23,9 +23,27 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET request for notes
 app.get('/api/notes', (req, res) => 
     res.json(db)
 );
+
+// GET a single note
+app.get('/api/notes/:title', (req, res) => {
+    if (req.params.title) {
+      const noteTitle = req.params.title;
+      for (let i = 0; i < notes.length; i++) {
+        const currentNote = notes[i];
+        if (currentNote.title === noteTitle) {
+            res.json(currentNote);
+            return;
+        }
+      }
+      res.status(404).send('Note not found');
+    } else {
+        res.status(400).send('Title not provided'); 
+    }
+});
 
 // Wildcard route to direct users to a home page
 app.get('*', (req, res) =>
