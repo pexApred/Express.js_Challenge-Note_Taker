815cb376bf8e25ef9c8b8a6a26d447d613cc9195
pexApred <120075705+pexApred@users.noreply.github.com>
2023-04-10 10:05:39 -0400

    finished with get routes, working on POST

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 // const express = require('express');
 const express = require('express');
 const path = require('path');
-// const { clog } = require('./middleware/clog');
-// const api = require('./routes/index.js');
+
 const PORT = process.env.PORT || 3001;
 
+let db = require('./db/db.json');
+
 const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Wildcard route to direct users to a 404 page
+app.get('/api/notes', (req, res) => 
+    res.json(db)
+);
+
+// Wildcard route to direct users to a home page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
